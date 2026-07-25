d5ef443f7786ad886972545264b88756244225f0
hibatullahbjn-debug <hibatullahbjn-debug@users.noreply.github.com>
2026-07-25 16:18:31 +0700

    Support multiple Manager login password options (manager & hibatullah manager)

--- push.bat
+++ push.bat
@@ -1,5 +1,5 @@
 @echo off
 set "PATH=C:\Program Files\Git\cmd;%PATH%"
 git add .
-git commit -m "Directly dispatch WhatsApp notification to Admin upon Konfirmasi click"
+git commit -m "Support multiple Manager login password options (manager & hibatullah manager)"
 git push origin main
