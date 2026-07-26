7dbade04882475e1f9488558934883be21057245
hibatullahbjn-debug <hibatullahbjn-debug@users.noreply.github.com>
2026-07-26 16:57:07 +0700

    Remove NOMOR WHATSAPP (WA) field from Inventaris profile view

--- push.bat
+++ push.bat
@@ -1,5 +1,5 @@
 @echo off
 set "PATH=C:\Program Files\Git\cmd;%PATH%"
 git add .
-git commit -m "Remove Hapus Semua Data button from Inventaris view"
+git commit -m "Remove NOMOR WHATSAPP (WA) field from Inventaris profile view"
 git push origin main
